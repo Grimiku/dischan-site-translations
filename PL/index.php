84b73e030bd10d1a8080b13f5a31b86cc899523c
Morthiasik <morthiasik@gmail.com>
2012-08-20 13:17:23 +0300

    Full index.php translation

--- PL/index.php
+++ PL/index.php
@@ -13,12 +13,12 @@ if (empty($siteStrings) || !is_array($siteStrings))
 //
 
 $siteStrings = array_merge($siteStrings, array(
-	'latestBlog'		=> 'Latest Blogposts',
-	'visitBlog'			=> 'visit the blog',
-	'readTheRest'		=> 'Read the rest of this post',
-	'featuredDL'		=> 'Featured Download',
+	'latestBlog'		=> 'Ostatnie wpisy na blogu',
+	'visitBlog'			=> 'odwiedź blog',
+	'readTheRest'		=> 'Czytaj dalej',
+	'featuredDL'		=> 'Do Pobrania',
 	'featuredDLTitle'	=> 'Juniper\'s Knot',
-	'featuredDLText'	=> 'Download Juniper\'s Knot here: <a href = "http://junipersknot.dischan.org/">official site</a>.',
+	'featuredDLText'	=> 'Pobierz Juniper\'s Knot tutaj: <a href = "http://junipersknot.dischan.org/">oficjalna strona</a>.',
 ));
 
 ?>
