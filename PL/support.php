1871e85cdda58e5e7316ae5208ffd38e2b2d8577
Morthiasik <morthiasik@gmail.com>
2012-09-04 19:54:51 +0300

    Full support.php translation

--- PL/support.php
+++ PL/support.php
@@ -13,11 +13,11 @@ if (empty($siteStrings) || !is_array($siteStrings))
 //
 
 $siteStrings = array_merge($siteStrings, array(
-	'support'				=> 'Support',
-	'supportPara'			=> '<p>Having trouble with a dischan product or service? If so, please check out the <a href = "http://www.dischan.org/forum/viewforum.php?c=3&f=8">support section of our forum</a>. If your
-								problem is not listed there, please feel free to start a new thread for it. Screenshots are appreciated.</p>
+	'support'				=> 'Wsparcie',
+	'supportPara'			=> '<p>Masz problemy z produktami Dischana lub serwisem? Jeśli tak, proszę odwiedzić <a href = "http://www.dischan.org/forum/viewforum.php?c=3&f=8">sekcję wsparcia na naszym forum</a>. Jeżeli 
+						szukane przez ciebie zagadnienie nie zostało jeszcze poruszone, rozpocznij nowy wątek. Zrzuty ekranu mile widziane!</p>
 								<br />
-								<p>For problems of a more sensitive nature, such as security issues, problems with your account, or store purchases, please use our <a href = "contact.php">contact form</a>.</p>',
+								<p>W przypadku problemów o bardziej wrażliwym charakterze, takich jak kwestie bezpieczeńtwa, kłopoty z kontem lub zakupy w sklepie, użyj naszego <a href = "contact.php">formularza kontaktowego</a>.</p>',
 ));
 
 ?>
